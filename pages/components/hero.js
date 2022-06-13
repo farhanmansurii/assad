f2686da5749e9d3ebfdce0c3e6d41e987439b474
farhan mansuri <mansurifarhanfm@gmail.com>
2022-06-12 17:22:45 -0700

    asd

--- pages/components/hero.js
+++ pages/components/hero.js
@@ -12,7 +12,7 @@ import dynamic from "next/dynamic";
 export default function Hero() {
   return (
     <>
-      <Flex justify="center" mx="5%" >
+      <Flex justify="center" mx="5%"  mt='4rem'>
         <Text
           mt="1rem"
           mb="3rem"
