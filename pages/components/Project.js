3a1f934d09bf07b653d50194306b9693b38c9e65
farhan mansuri <mansurifarhanfm@gmail.com>
2022-06-14 05:21:26 -0700

    sa

--- pages/components/Project.js
+++ pages/components/Project.js
@@ -12,14 +12,14 @@ import Link from "next/link";
 export default function Project() {
   return (
     <>
-      <Container w="350px" p="5">
+      <Container w="400px" p="5">
         <Image
           src="https://images.unsplash.com/photo-1653894604359-22dbdb074d4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80"
           borderRadius="xl"
           alt="hi"
         />
         <Flex direction="column" my="1rem">
-          <Text fontSize="lg"> {title}</Text>
+          <Text fontSize="lg"> Title</Text>
           <Text fontSize="md">Description</Text>
           <Flex fontSize="md" my="0.5rem" justify="space-between">
             <Text> html css javascript </Text>
--- pages/work.js
+++ pages/work.js
@@ -2,10 +2,7 @@ import { Text, Flex, SimpleGrid, Container } from "@chakra-ui/react";
 import Link from "next/link";
 import { AiFillGithub, AiOutlineLink } from "react-icons/ai";
 import React from "react";
-import Image from "next/image";
-import github from "./components/data/images/github2.png";
 import Project from "./components/Project";
-import { projectsData } from "./components/data/projects";
 
 export default function work() {
   return (
@@ -13,28 +10,11 @@ export default function work() {
       <Text textAlign="center" fontSize="2xl">
         {" "}
         Works ive done
+        <Project />
+        <Project />
+        <Project />
+        <Project />
       </Text>
-      {projectsData &&
-        projectsData.map((n) => (
-          <Container key={n.id} w="400px" p="5">
-            <Image src={github} borderRadius="xl" alt="hi" />
-            <Flex direction="column" my="1rem" spacing="1">
-              <Text fontSize="lg"> {n.title}</Text>
-              <Text fontSize="sm">{n.description}</Text>
-              <Flex fontSize="md" my="0.5rem" justify="space-between">
-                <Text> {n.technologies} </Text>
-                <Flex gap="2">
-                  <Link href="/">
-                    <AiFillGithub size="25px" />
-                  </Link>
-                  <Link href="/">
-                    <AiOutlineLink size="25px" />
-                  </Link>
-                </Flex>
-              </Flex>
-            </Flex>
-          </Container>
-        ))}
     </Flex>
   );
 }
