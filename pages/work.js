7ad9dfdd4fa32ff3c5e2d0d4e360326fd830b277
farhan mansuri <mansurifarhanfm@gmail.com>
2022-06-14 16:01:31 -0700

    main

--- pages/work.js
+++ pages/work.js
@@ -1,20 +1,44 @@
-import { Text, Flex, SimpleGrid, Container } from "@chakra-ui/react";
+import { Text, Flex, Container, Box, Image } from "@chakra-ui/react";
+import React from "react";
+import projects from "./components/data/images/projects.json";
+import Project from "./components/Project";
 import Link from "next/link";
 import { AiFillGithub, AiOutlineLink } from "react-icons/ai";
-import React from "react";
-import Project from "./components/Project";
-
 export default function work() {
   return (
-    <Flex direction="column" mt="5rem" spacing={3}>
-      <Text textAlign="center" fontSize="2xl">
-        {" "}
-        Works ive done
-        <Project />
-        <Project />
-        <Project />
-        <Project />
-      </Text>
-    </Flex>
+    <>
+      <Flex direction="column" mt="5rem" spacing={3}>
+        <Text textAlign="center" fontSize="2xl">
+          Works ive done
+        </Text>
+        {projects.map((project) => {
+          return (
+            <Container w="400px" p="5">
+              <Image
+                src="https://images.unsplash.com/photo-1653894604359-22dbdb074d4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80"
+                borderRadius="xl"
+                alt="hey
+            "
+              />
+              <Flex direction="column" my="1rem">
+                <Text fontSize="lg"> {project.title}</Text>
+                <Text fontSize="md">{project.description}</Text>
+                <Flex fontSize="md" my="0.5rem" justify="space-between">
+                  <Text> {project.tools} </Text>
+                  <Flex gap="2">
+                    <Link href={project.live}>
+                      <AiOutlineLink size="25px" />
+                    </Link>
+                    <Link href="/">
+                      <AiFillGithub size="25px" />
+                    </Link>
+                  </Flex>
+                </Flex>
+              </Flex>
+            </Container>
+          );
+        })}
+      </Flex>
+    </>
   );
 }
